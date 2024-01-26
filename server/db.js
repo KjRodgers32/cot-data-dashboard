b3a8feb9fe1d139572cbebc526c8dd89a626d2cc
Kevin <kjrodgers26@gmail.com>
2024-01-26 09:39:34 -0500

    chaned db to vercel postgres

--- server/db.js
+++ server/db.js
@@ -1,13 +1,25 @@
-const { Pool } = require("pg");
-const dotenv = require("dotenv");
+// const { Pool } = require("pg");
+// const dotenv = require("dotenv");
 
-dotenv.config({ path: "./.env" });
+// dotenv.config({ path: "./.env" });
+
+// const pool = new Pool({
+// 	user: process.env.POSTGRES_USER,
+// 	host: process.env.POSTGRES_HOST,
+// 	port: process.env.DB_PORT,
+// 	database: process.env.POSTGRES_DATABASE,
+// });
+
+// const db = {
+// 	query: (text, params) => pool.query(text, params),
+// };
+
+// module.exports = { db };
+
+const { Pool } = require("pg");
 
 const pool = new Pool({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DB_DATABASE,
+	connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 const db = {

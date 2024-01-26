fded504c6b44b0000d93500190345bc7e181923f
Kevin <kjrodgers26@gmail.com>
2024-01-26 10:22:45 -0500

    changed db to render

--- server/db.js
+++ server/db.js
@@ -1,28 +1,13 @@
-const { Pool } = require("pg");
-const dotenv = require("dotenv");
-
-dotenv.config({ path: "./.env" });
-
-const pool = new Pool({
-	user: process.env.POSTGRES_USER,
-	host: process.env.POSTGRES_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.POSTGRES_DATABASE,
-});
-
-const db = {
-	query: (text, params) => pool.query(text, params),
-};
-
-module.exports = { db };
-
 // const { Pool } = require("pg");
+// const dotenv = require("dotenv");
+
+// dotenv.config({ path: "./.env" });
 
 // const pool = new Pool({
-// 	connectionString: process.env.POSTGRES_URL,
-// 	ssl: {
-// 		rejectUnauthorized: false,
-// 	},
+// 	user: process.env.POSTGRES_USER,
+// 	host: process.env.POSTGRES_HOST,
+// 	port: process.env.DB_PORT,
+// 	database: process.env.POSTGRES_DATABASE,
 // });
 
 // const db = {
@@ -30,3 +15,18 @@ module.exports = { db };
 // };
 
 // module.exports = { db };
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+	connectionString: process.env.POSTGRES_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
+});
+
+const db = {
+	query: (text, params) => pool.query(text, params),
+};
+
+module.exports = { db };
